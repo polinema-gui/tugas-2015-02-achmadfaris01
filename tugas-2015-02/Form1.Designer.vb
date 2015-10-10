af46647568b21d1b3fd3ae1ff43a600747d4f041
achmadfaris01 <achmad.fariiz@gmail.com>
2015-10-10 22:12:42 +0700

    tugas kalkulator

--- tugas-2015-02/Form1.Designer.vb
+++ tugas-2015-02/Form1.Designer.vb
@@ -22,9 +22,201 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.ButtonClear = New System.Windows.Forms.Button()
+        Me.ButtonMin = New System.Windows.Forms.Button()
+        Me.Button0 = New System.Windows.Forms.Button()
+        Me.ButtonSamadengan = New System.Windows.Forms.Button()
+        Me.ButtonPlus = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(13, 13)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.TextBox1.Size = New System.Drawing.Size(215, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "0"
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(13, 48)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(49, 41)
+        Me.Button7.TabIndex = 1
+        Me.Button7.Text = "7"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(68, 48)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(48, 41)
+        Me.Button8.TabIndex = 2
+        Me.Button8.Text = "8"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(122, 48)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(49, 41)
+        Me.Button9.TabIndex = 3
+        Me.Button9.Text = "9"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(13, 103)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(49, 40)
+        Me.Button4.TabIndex = 4
+        Me.Button4.Text = "4"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(68, 103)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(48, 40)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "5"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(122, 103)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(49, 40)
+        Me.Button6.TabIndex = 6
+        Me.Button6.Text = "6"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(13, 157)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(49, 42)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(68, 157)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(48, 42)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "2"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(122, 157)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(49, 42)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "3"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'ButtonClear
+        '
+        Me.ButtonClear.Location = New System.Drawing.Point(179, 48)
+        Me.ButtonClear.Name = "ButtonClear"
+        Me.ButtonClear.Size = New System.Drawing.Size(49, 103)
+        Me.ButtonClear.TabIndex = 10
+        Me.ButtonClear.Text = "Clear"
+        Me.ButtonClear.UseVisualStyleBackColor = True
+        '
+        'ButtonMin
+        '
+        Me.ButtonMin.Location = New System.Drawing.Point(179, 157)
+        Me.ButtonMin.Name = "ButtonMin"
+        Me.ButtonMin.Size = New System.Drawing.Size(49, 42)
+        Me.ButtonMin.TabIndex = 11
+        Me.ButtonMin.Text = "-"
+        Me.ButtonMin.UseVisualStyleBackColor = True
+        '
+        'Button0
+        '
+        Me.Button0.Location = New System.Drawing.Point(13, 208)
+        Me.Button0.Name = "Button0"
+        Me.Button0.Size = New System.Drawing.Size(49, 42)
+        Me.Button0.TabIndex = 12
+        Me.Button0.Text = "0"
+        Me.Button0.UseVisualStyleBackColor = True
+        '
+        'ButtonSamadengan
+        '
+        Me.ButtonSamadengan.Location = New System.Drawing.Point(68, 208)
+        Me.ButtonSamadengan.Name = "ButtonSamadengan"
+        Me.ButtonSamadengan.Size = New System.Drawing.Size(103, 42)
+        Me.ButtonSamadengan.TabIndex = 13
+        Me.ButtonSamadengan.Text = "="
+        Me.ButtonSamadengan.UseVisualStyleBackColor = True
+        '
+        'ButtonPlus
+        '
+        Me.ButtonPlus.Location = New System.Drawing.Point(179, 208)
+        Me.ButtonPlus.Name = "ButtonPlus"
+        Me.ButtonPlus.Size = New System.Drawing.Size(49, 42)
+        Me.ButtonPlus.TabIndex = 14
+        Me.ButtonPlus.Text = "+"
+        Me.ButtonPlus.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(233, 262)
+        Me.Controls.Add(Me.ButtonPlus)
+        Me.Controls.Add(Me.ButtonSamadengan)
+        Me.Controls.Add(Me.Button0)
+        Me.Controls.Add(Me.ButtonMin)
+        Me.Controls.Add(Me.ButtonClear)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Kalkulator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents ButtonClear As System.Windows.Forms.Button
+    Friend WithEvents ButtonMin As System.Windows.Forms.Button
+    Friend WithEvents Button0 As System.Windows.Forms.Button
+    Friend WithEvents ButtonSamadengan As System.Windows.Forms.Button
+    Friend WithEvents ButtonPlus As System.Windows.Forms.Button
 
 End Class
